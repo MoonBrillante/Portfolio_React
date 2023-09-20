1fe020da478e245dbdfdf74074fa8f4752687a20
yueming <bluemoon_cn@hotmail.com>
2023-09-20 23:11:01 +0200

    review

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,19 +15,19 @@ const Hero = () => {
           
         <div className="about-content">
           <h2 className="heading">Sobre <span>Mi</span></h2>
-          <h3>Desarrollador Junior Full Stack</h3>
+          <h3>Desarrolladora Junior Full Stack</h3>
             <p>Soy una desarrolladora junior Full Stack formada en Factoría F5.</p>
             <span class="aboutme"><LiaLaptopCodeSolid size={20}/>Front End</span>
-            <p>Tengo un sólido conocimiento de tecnologías de frontend como HTML, CSS y JavaScript. Tengo experiencia con React para construir diseños dinámicos, interactivos y animados</p> 
+            <p>Poseo un sólido conocimiento de tecnologías de frontend como HTML, CSS y JavaScript. Tengo experiencia con React para construir diseños dinámicos, interactivos y animados</p> 
             <span class="aboutme"><HiOutlineCode size={20}/>Back end</span>
             <p>Tengo experiencia con Java Spring Boot para el desarrollo de APIs. Además, poseo amplio conocimiento y capacidad en el trabajo con Python y Django para crear blogs.</p>
             <span class="aboutme"><SiFuturelearn size={15}/>Autodidacta</span>
-            <p>Me formado a partir de libros y utilizando recursos en línea, incluyendo búsquedas en Google, obteniendo los certificados correspondientes, interpretación de mensajes de error y comprensión de documentación.</p>
+            <p>Una parte importante de mi formación la obtuve a partir de libros y utilizando recursos en línea, incluyendo búsquedas en Google, obteniendo los certificados correspondientes, interpretación de mensajes de error y comprensión de documentación.</p>
             <span class="aboutme"><DiScrum size={25}/>Trabajo en equipo</span>
             <p>Mis experiencias más gratificantes en programación implican trabajar con otras personas. Creo firmemente en el poder de las sinergias de las personas de un equipo.</p>
           </div>
       </section>
       
 );
-}
+
 export default Hero;
